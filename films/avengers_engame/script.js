e80387385300266340ad576a0d14a109c6ed9f11
gamefelix <123744766+gamefelix@users.noreply.github.com>
2024-09-18 21:59:00 +0200

    Delete films/avengers_engame directory

--- films/avengers_engame/script.js
+++
@@ -1,4 +0,0 @@
-// Funkce, která spustí akci po kliknutí na tlačítko "Přehrát"
-document.getElementById("customButton").addEventListener("click", function() {
-    window.open("https://ovh-s1-storage1.premiumcdn.net/60643877/dndU6MI4puyNurGtxAfkxIaKhZXsdVF79t5yFOAf0DcyxsAyuG6UpJtAlc7pCV6hvxhmJi8qUz0TzriKOiQOhz7TRpu1oHp7Fyki7ksd1FgkH4gfRYsyr.mp4?token=A1206ZyNVCax&expires=1726771621&sparams=token%2Cpath%2Cexpires&signature=9ca115905b87bb3af6d810a7eaee9ceb1edfdcfc", "_blank");
-});
